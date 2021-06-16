2bc7267efc1e5f1b4e0011803f6612997ee77213
Shehanka <hcsperera@outlook.com>
2021-06-16 06:32:56 +0530

    Users List

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
@@ -8,7 +9,18 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Working'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          MaterialButton(
+            onPressed: () {
+              Modular.to.navigate('/users');
+            },
+            child: Text('Users list'),
+          )
+        ],
+      ),
     );
   }
 }
